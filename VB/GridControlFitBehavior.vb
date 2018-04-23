2c02bd8f7a901b8d7eae86d17ae462f72a1c9263
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/GridControlFitBehavior.vb
@@ -0,0 +1,103 @@
+﻿Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Mvvm.UI.Interactivity
+Imports System
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Collections.Specialized
+Imports System.ComponentModel
+Imports System.Linq
+Imports System.Text
+
+Namespace E2042
+
+    Public Class GridControlFitBehavior
+        Inherits Behavior(Of GridControl)
+
+        Protected Overrides Sub OnAttached()
+            MyBase.OnAttached()
+            AddHandler Me.AssociatedObject.ItemsSourceChanged, AddressOf AssociatedObject_ItemsSourceChanged
+            AddHandler Me.AssociatedObject.Loaded, AddressOf AssociatedObject_Loaded
+        End Sub
+
+        Private Sub AssociatedObject_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim collection = Me.AssociatedObject.ItemsSource
+            Me.AssociatedObject.ItemsSource = Nothing
+            Me.AssociatedObject.ItemsSource = collection
+            If (TryCast(Me.AssociatedObject.View, TableView)) IsNot Nothing Then
+                TryCast(Me.AssociatedObject.View, TableView).BestFitColumns()
+            End If
+        End Sub
+
+        Protected Overrides Sub OnDetaching()
+            Me.UnSubscribeCollectionChanged(Me.AssociatedObject.ItemsSource)
+            Me.UnSubscribeItemChanged(Me.AssociatedObject.ItemsSource)
+            RemoveHandler Me.AssociatedObject.ItemsSourceChanged, AddressOf AssociatedObject_ItemsSourceChanged
+            RemoveHandler Me.AssociatedObject.Loaded, AddressOf AssociatedObject_Loaded
+            MyBase.OnDetaching()
+        End Sub
+
+        Private Sub AssociatedObject_ItemsSourceChanged(ByVal sender As Object, ByVal e As ItemsSourceChangedEventArgs)
+            If e.OldItemsSource IsNot Nothing Then
+                UnSubscribeItemChanged(e.OldItemsSource)
+                UnSubscribeCollectionChanged(e.OldItemsSource)
+            End If
+            If e.NewItemsSource IsNot Nothing Then
+                SubscribeItemChanged(e.NewItemsSource)
+                SubscribeCollectionChanged(e.NewItemsSource)
+            End If
+            If (TryCast(Me.AssociatedObject.View, TableView)) IsNot Nothing Then
+                TryCast(Me.AssociatedObject.View, TableView).BestFitColumns()
+            End If
+        End Sub
+
+
+        Public Sub SubscribeCollectionChanged(ByVal source As Object)
+            Dim notifyCollection = TryCast(source, INotifyCollectionChanged)
+            If notifyCollection IsNot Nothing Then
+                AddHandler notifyCollection.CollectionChanged, AddressOf notifyCollection_CollectionChanged
+            End If
+        End Sub
+
+        Public Sub UnSubscribeCollectionChanged(ByVal source As Object)
+            Dim notifyCollection = TryCast(source, INotifyCollectionChanged)
+            If notifyCollection IsNot Nothing Then
+                RemoveHandler notifyCollection.CollectionChanged, AddressOf notifyCollection_CollectionChanged
+            End If
+        End Sub
+
+        Public Sub SubscribeItemChanged(ByVal source As Object)
+            Dim collection = TryCast(source, IList)
+            For Each obj As Object In collection
+                Dim castObject = TryCast(obj, INotifyPropertyChanged)
+                If castObject IsNot Nothing Then
+                    AddHandler castObject.PropertyChanged, AddressOf castObject_PropertyChanged
+                End If
+            Next obj
+        End Sub
+
+        Public Sub UnSubscribeItemChanged(ByVal source As Object)
+            Dim collection = TryCast(source, IList)
+            For Each obj As Object In collection
+                Dim castObject = TryCast(obj, INotifyPropertyChanged)
+                If castObject IsNot Nothing Then
+                    RemoveHandler castObject.PropertyChanged, AddressOf castObject_PropertyChanged
+                End If
+            Next obj
+        End Sub
+
+        Private Sub notifyCollection_CollectionChanged(ByVal sender As Object, ByVal e As NotifyCollectionChangedEventArgs)
+            If e.NewItems IsNot Nothing Then
+                SubscribeItemChanged(e.NewItems)
+            End If
+            If e.OldItems IsNot Nothing Then
+                UnSubscribeItemChanged(e.OldItems)
+            End If
+            TryCast(Me.AssociatedObject.View, TableView).BestFitColumns()
+        End Sub
+
+
+        Private Sub castObject_PropertyChanged(ByVal sender As Object, ByVal e As PropertyChangedEventArgs)
+            TryCast(Me.AssociatedObject.View, TableView).BestFitColumns()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/Task.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Linq
+Imports System.Text
+
+Namespace E2042
+    Public Class Task
+        Implements INotifyPropertyChanged
+
+        Private _Num As Integer
+        Private _Name As String
+        Private _IsCompleted As Boolean
+
+        Public Property Num() As Integer
+            Get
+                Return _Num
+            End Get
+            Set(ByVal value As Integer)
+                _Num = value
+                NotifyPropertyChanged("Num")
+            End Set
+        End Property
+
+
+        Public Property Name() As String
+            Get
+                Return _Name
+            End Get
+            Set(ByVal value As String)
+                _Name = value
+                NotifyPropertyChanged("Name")
+            End Set
+        End Property
+
+        Public Property IsCompleted() As Boolean
+            Get
+                Return _IsCompleted
+            End Get
+            Set(ByVal value As Boolean)
+                _IsCompleted = value
+                NotifyPropertyChanged("IsCompleted")
+            End Set
+        End Property
+
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Private Sub NotifyPropertyChanged(ByVal propertyName As String)
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+    End Class
+End Namespace
