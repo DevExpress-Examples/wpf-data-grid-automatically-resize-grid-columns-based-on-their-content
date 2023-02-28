db2f3e1631fc3c265b73e1de39f9727425dcc476
AndreySlabov <andrey.slabov@devexpress.com>
2023-02-28 14:37:07 +0400

    Update Sample

--- CS/Task.cs
+++ CS/Task.cs
@@ -1,52 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
+﻿using DevExpress.Mvvm;
 
 namespace E2042 {
-    public class Task : INotifyPropertyChanged {
-        private int _Num;
-        private string _Name;
-        private bool _IsCompleted;
-
-        public int Num {
-            get {
-                return _Num;
-            }
-            set {
-                _Num = value;
-                NotifyPropertyChanged("Num");
-            }
-        }
-
-
-        public string Name {
-            get {
-                return _Name;
-            }
-            set {
-                _Name = value;
-                NotifyPropertyChanged("Name");
-            }
-        }
-
-        public bool IsCompleted {
-            get {
-                return _IsCompleted;
-            }
-            set {
-                _IsCompleted = value;
-                NotifyPropertyChanged("IsCompleted");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void NotifyPropertyChanged(string propertyName) {
-            if(PropertyChanged != null) {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+    public class Task : BindableBase {
+        public int Num { get { return GetValue<int>(); } set { SetValue(value); } }
+        public string Name { get { return GetValue<string>(); } set { SetValue(value); } }
+        public bool IsCompleted { get { return GetValue<bool>(); } set { SetValue(value); } }
     }
 }
--- CS/ViewModel.cs
+++ CS/ViewModel.cs
@@ -1,93 +1,46 @@
 ﻿using DevExpress.Mvvm;
-using System;
-using System.Collections.Generic;
+using DevExpress.Mvvm.DataAnnotations;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
-namespace E2042
-{
-    public class ViewModel : INotifyPropertyChanged
-    {
-        public ViewModel()
-        {
-            this.SetNewData();
-            this.ChangeItem = new DelegateCommand(this.ChangeItemExecute);
-            this.ChangeSource = new DelegateCommand(this.ChangeSourceExecute);
-            this.DeleteItem = new DelegateCommand(this.DeleteItemExecute);
-            this.AddNewItem = new DelegateCommand(this.AddNewItemExecute);
+namespace E2042 {
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            SetNewData();
         }
-
-        private ObservableCollection<Task> _List;
-        public ObservableCollection<Task> List
-        {
-            get
-            {
-                return _List;
-            }
-
-            set
-            {
-                if(this._List != value) {
-                    this._List = value;
-                    this.OnPropertyChanged("List");
-                }
-                
-            }
-        }
-
-        public ICommand ChangeSource { get; set; }
-        public ICommand AddNewItem { get; set; }
-        public ICommand DeleteItem { get; set; }
-        public ICommand ChangeItem { get; set; }
-
-        protected void ChangeSourceExecute() {
-            this.SetNewData();
-        }
-
-        protected void AddNewItemExecute() {
-            if(this.List != null) {
-                this.List.Add(new Task() { Num = 0, Name = "Additional New Item" });
-            }
-            
-        }
-
-        protected void DeleteItemExecute() {
-            if(this.List != null && this.List.Count > 0) {
-                this.List.RemoveAt(this.List.Count - 1);
-            }
-        }
-
-        protected void ChangeItemExecute() {
-            if(this.List != null && this.List.Count > 0) {
-                this.List[0].Num = 1000000000;
-            }
-        }
-
-        protected void SetNewData() {
-            if(this.List == null) {
+        public ObservableCollection<Task> List { get; set; }
+        public void SetNewData() {
+            if (List == null) {
                 List = new ObservableCollection<Task>();
             } else {
-                this.List.Clear();
+                List.Clear();
             }
-            for(int i = 0; i < 20; i++) {
+            for (int i = 0; i < 20; i++) {
                 List.Add(new Task() { Num = i, Name = "Name " + i * (i + 10), IsCompleted = i % 2 == 0 });
             }
         }
 
-
-
-        public void OnPropertyChanged(string info) {
-            if(this.PropertyChanged != null) {
-                this.PropertyChanged(this, new PropertyChangedEventArgs(info));
+        [Command]
+        public void ChangeSource() {
+            SetNewData();
+        }
+        [Command]
+        public void ChangeItem() {
+            if (List != null && List.Count > 0) {
+                List[0].Num = 1000000000;
             }
         }
+        [Command]
+        public void AddNewItem() {
+            if (List != null) {
+                List.Add(new Task() { Num = 0, Name = "Additional New Item" });
+            }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        }
+        [Command]
+        public void DeleteItem() {
+            if (List != null && List.Count > 0) {
+                List.RemoveAt(List.Count - 1);
+            }
+        }
     }
-
 }
